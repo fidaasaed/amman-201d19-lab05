150eb3feaaba1e35a1f2d7cef126798a27cbe860
fidaasaed <fidaa.saed97@gmail.com>
2021-03-20 08:59:14 +0200

    function4 solved

--- app.js
+++ app.js
@@ -49,22 +49,20 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-            let sum = a+b+c;
-            let mul = a*b*c;
-             return [sum,mul,+a+' and '+b+' and '+c+' sum to '+sum+'.','The product of '+a+' and '+b+' and '+c+' is '+mul+'.'];
+     let sum = a+b+c;
+     let mul = a*b*c;
+     return [sum,mul,+a+' and '+b+' and '+c+' sum to '+sum+'.','The product of '+a+' and '+b+' and '+c+' is '+mul+'.'];
          }
         
-         console.log(sumAndMultiply(5,7,2)); 
-         testSumAndMultiply(4,7,5);
+    console.log(sumAndMultiply(5,7,2)); 
+
+    
         
         
 
-
-
-}
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -82,12 +80,15 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+    let result = Number(sum(sumArr[0],sumArr[1],sumArr[2]));
+    return [result,sumArr+' was passed in as an array of numbers, and '+result+' is their sum.'];
 }
+console.log(sumArray(testArray));
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
